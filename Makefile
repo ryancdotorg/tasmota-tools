b2174be0a23558226b71c5afd6dacf0b8acf53f1
Ryan Castellucci <code@ryanc.org>
2024-03-03 11:33:07 +0000

    golf++

--- Makefile
+++ Makefile
@@ -3,9 +3,11 @@ MAKEFLAGS += --no-builtin-rules
 .PHONY: all clean _clean _nop
 
 %.min.js: %.js
-	terser $< \
-		--emca 6 -m -o $@ \
+	sed -E 's/\b(let|const)\b/var/g' $< | \
+	terser \
+		--ecma 6 -f ascii_only=true -m -o $@ \
 		-c passes=3,unsafe=true,booleans_as_integers=true,drop_console=true
+	@printf '\n' >> $@
 
 cli.js: atob.js tasmota_fingerprint.js readfile.js
 	cat $^ > $@
--- tasmota_fingerprint.js
+++ tasmota_fingerprint.js
@@ -3,71 +3,79 @@
 
 const tasmota_tls_fingerprint = (_=>{
   // golfed SHA1, reuses variables for size; the data has to be already padded
-  // based on an old version of https://github.com/jbt/tiny-hashes
-  const raw_sha1 = u8 => {
-    for (var blockstart=0,
-        i = 0,
-        W = [],
-        A, B, C, D, F, G,
-        H = [A = 0x67452301, B = 0xEFCDAB89, ~A, ~B, 0xC3D2E1F0],
-        words = [],
-        n = u8.length;
-      i<n;){
-      words[i>>2] |= (u8[i])<<(8*(3-i++%4));
+  // based on https://github.com/jbt/tiny-hashes/blob/04cfef0/sha1/sha1.js
+  const raw_sha1 = (u, j) => {
+    var
+      i,
+      W = [],
+      words = [],
+      A, B, C, D,
+      n = --j >> 2,
+      H = [A = 0x67452301, B = 0xEFCDAB89, ~A, ~B, 0xC3D2E1F0];
+
+    for (; ~j;) { // j !== -1
+      words[j >> 2] |= u[j] << 8 * ~j--;
     }
 
-    n /= 4;
+    for (i = j = 0; i < n; i += 16) {
+      A = H;
 
-    for (; blockstart < n; blockstart += 16) {
-      A = H, i = 0;
-
-      for (; i < 80;
-        A = [[
-          (G = ((u8=A[0])<<5|u8>>>27) + A[4] + (W[i] = (i<16) ? ~~words[blockstart + i] : G*2|G<0) + 1518500249) + ((B=A[1]) & (C=A[2]) | ~B & (D=A[3])),
-          F = G + (B ^ C ^ D) + 341275144,
-          G + (B & C | B & D | C & D) + 882459459,
-          F + 1535694389
-        ][i++/5>>2] | 0, u8, B<<30|B>>>2, C, D]
+      for (; j < 80;
+        A = [
+          (
+            A[4] +
+            (
+              W[j] =
+                (j < 16)
+                  ? ~~words[i + j]
+                  : u * 2 | u < 0 // u << 1 | s >>> 31
+            ) +
+            1518500249 +
+            [
+              (B & C | ~B & D),
+              u = (B ^ C ^ D) + 341275144,
+              (B & C | B & D | C & D) + 882459459,
+              u + 1535694389
+            ][j++ / 5 >> 2] +
+            ((u = A[0]) << 5 | u >>> 27)
+          ),
+          u,
+          B << 30 | B >>> 2,
+          C,
+          D
+        ]
       ) {
-        G = W[i - 3] ^ W[i - 8] ^ W[i - 14] ^ W[i - 16];
+        u = W[j - 3] ^ W[j - 8] ^ W[j - 14] ^ W[j - 16];
+        B = A[1];
+        C = A[2];
+        D = A[3];
       }
 
-      for (i = 5; i;) {
-        H[--i] += A[i];
+      for (j = 5; j;) {
+        H[--j] += A[j];
       }
     }
 
     // hex encode with a space between each byte
-    for (u8 = ''; i < 40;) {
-      u8 += (i && !(i&1) ? ' ' : '') +
-            (H[i >> 3] >> (7 - i++) * 4 & 15).toString(16);
+    for (u8 = '', j = 0; j < 40;) {
+      u8 += (j && !(j&1) ? ' ' : '') +
+            (H[j >> 3] >> (7 - j++) * 4 & 15).toString(16);
     }
 
     return u8.toUpperCase();
   };
 
   // base64 decode to Uint8Array
-  const b64d = str => {
-    // atob returns the byte values in a string... :-(
-    str = atob(str);
-
-    for (var n = str.length, u8 = new Uint8Array(n); n;) {
-      u8[--n] = str.charCodeAt(n);
-    }
-
-    return u8;
-  };
+  const b64d = str => Uint8Array.from(atob(str), c => c.charCodeAt());
 
   // the OID for rsaEncryption
   const rsaEncryption = b64d('KoZIhvcNAQEB');
 
-  // check whether two Uint8Arrays are equal
+  // check whether two arrays are equal (sloppy compare)
   // XXX not constant time
-  const u8Equal = (a, b) => {
-    let n = a.length, ret = n ^ b.length;
-    while (!ret && n--) { ret = a[n] ^ b[n]; }
-    return !ret;
-  };
+  const u8Equal = (a, b) =>
+    a.length == b.length &&
+    a.every((v, i) => v == b[i]);
 
   // strip e.g. -----BEGIN PUBLIC KEY--- and base64 decode
   const pemToDer = str => b64d(str.replace(/(?:-+[^-]+-+|\n)/gm, ''));
@@ -78,11 +86,11 @@ const tasmota_tls_fingerprint = (_=>{
 
     // first two bits are class, next is form, last five are number
     //let class_ = u8[off] >> 6;
-    let form = (u8[off] >> 5) & 1;
+    let form = u8[off] & 32;
     let type = u8[off++] & 31;
 
     // values larger than 30 are encoded in additional bytes
-    if (type === 31) {
+    if (type == 31) {
       type = 0;
       do {
         tmp = u8[off++];
@@ -114,28 +122,28 @@ const tasmota_tls_fingerprint = (_=>{
     const resultU8 = new Uint8Array(2048);
     const resultDV = new DataView(resultU8.buffer);
     let der = pemToDer(data);
-    let offset = 0, toSave = 0;
+    let n, i, offset = 0, toSave_blockEnd;
 
     // crudely parse the ASN.1 data
     while (offset < der.length) {
       const [new_offset, form, type, len, value] = getAsn1TLV(der, offset);
-      console.log(der.length, offset, new_offset, toSave, form, type, len, value.toString());
+      //console.log(der.length, offset, new_offset, toSave_blockEnd, form, type, len, value.toString());
 
       // we discard everyting until we find an rsa public key oui
       if (len && !form && type != 3) {
-        if (toSave < 1) {
-          toSave = u8Equal(value, rsaEncryption) && 2;
+        if (!toSave_blockEnd) {
+          toSave_blockEnd = u8Equal(value, rsaEncryption) * 2;
         } else {
           // save the rsa public key data... if there's a leading zero, it
           // needs to be removed for compatibility with Tasmota and BearSSL
-          pubKeyData[toSave] = value.slice(value[0] ? 0 : 1);
+          pubKeyData[toSave_blockEnd] = value.slice(!value[0]); // (value[0] ? 0 : 1);
           // we're done once we have the key
-          if (!(--toSave)) { break; }
+          if (!(--toSave_blockEnd)) { break; }
         }
       }
 
       // skip over the data unles the form bit is set
-      offset = new_offset + (form ? 0 : len);
+      offset = new_offset + !form * len; // (form ? 0 : len);
       // type 3 is "BIT STRING" and should contain the key as ASN.1 data
       if (type == 3) {
         // need to skip the first byte, not sure why
@@ -146,10 +154,9 @@ const tasmota_tls_fingerprint = (_=>{
     }
 
     // serialize the public key in tasmota's "new" format
-    offset = 0;
-    for (let n, i = 0; i < 3; ++i) {
+    for (offset = 0, i = 0; i < 3; ++i) {
       // 4 byte big endian length
-      resultDV.setUint32(offset, n = pubKeyData[i].length, false);
+      resultDV.setUint32(offset, n = pubKeyData[i].length);
       offset += 4;
       // actual data
       resultU8.set(pubKeyData[i], offset);
@@ -158,10 +165,10 @@ const tasmota_tls_fingerprint = (_=>{
 
     // add SHA1 padding
     resultU8[offset] = 0x80;
-    let blockEnd = offset + 72 & ~63;
-    resultDV.setUint32(blockEnd-4, offset * 8, false);
+    toSave_blockEnd = offset + 72 & ~63;
+    resultDV.setUint32(toSave_blockEnd-4, offset * 8);
 
-    // truncate and hash
-    return raw_sha1(resultU8.slice(0, blockEnd));
+    // hash
+    return raw_sha1(resultU8, toSave_blockEnd);
   };
 })();
